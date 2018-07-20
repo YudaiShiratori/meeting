a5cffe18b73b1db8f61e2c83295b5b3ce8dba763
Yudai <toritorikebabu1023x@gmail.com>
2018-07-20 04:57:54 +0000

    start_bootstrap top_page

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,10 @@ class User < ApplicationRecord
   has_many :pictures, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_pictures, through: :favorites, source: :picture
+  
+  has_many :following, through: :active_relationship, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
 end
