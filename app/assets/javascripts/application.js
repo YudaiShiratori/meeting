f0379c8ac6f4a4f413ffcb9e8f7c21b34bbc97c4
Yudai <toritorikebabu1023x@gmail.com>
2018-09-08 04:41:31 +0000

    calendar size修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,19 @@
 //= require fullcalendar/locale-all
 //= require_tree .
 
+
+$(document).ready(function() {
+  $('#calendar').fullCalendar({
+        header:{
+            left:'prev,next,today',
+            center:'title',
+            right:'month agendaWeek agendaDay'
+        },
+        buttonText: {
+            month:'月',
+            week: '週',
+            day:'日'
+        }
+  });
+});
+   
